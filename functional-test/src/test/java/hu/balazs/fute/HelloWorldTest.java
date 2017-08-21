53f107f863361238fa2a11ddfd17c41568936e3e
blzsaa <balazs.veres@yahoo.com>
2017-08-21 16:53:25 +0200

    add skeleton

---
+++ functional-test/src/test/java/hu/balazs/fute/HelloWorldTest.java
@@ -0,0 +1,41 @@
+package hu.balazs.fute;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import demo.ImdbListComparatorApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {ImdbListComparatorApplication.class},
+    webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HelloWorldTest {
+
+  @Autowired
+  private WebApplicationContext context;
+
+  private MockMvc mvc;
+
+  @Before
+  public void setup() {
+    mvc = MockMvcBuilders.webAppContextSetup(context).build();
+  }
+
+  @Test
+  public void testVote() throws Exception {
+    mvc.perform(get("/")).andExpect(content().string(containsString("Hello world!")))
+        .andExpect(status().isOk());
+  }
+
+}
